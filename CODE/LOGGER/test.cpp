541af7e63cdea25f4323abd7c6e79ce975f97cd6
gayathri <gayathripathivada11@gmail.com>
2022-11-19 05:24:20 -0500

    Creation of testfile

---
+++ CODE/LOGGER/test.cpp
@@ -0,0 +1,17 @@
+#include <assert.h>
+
+#include <thread>
+
+
+int main()
+{
+        const char* name = "User";
+        LOG_INFO("Hello ****WELCOME TO CHAT BOX APPLICATION****%s", name);
+
+        LOG_CRITICAL("OH NO!", name);
+
+        LOG_WARN("Hello %d %d", 15, 28);
+
+        return 0;
+}
+
